8247d290932420f139fa8abc21b28b78d429c49e
Henrique S.A <henriqueator@gmail.com>
2020-07-16 12:44:55 -0300

    Card component - JSX and styles.css 's $ Understing props and children props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
@@ -19,9 +20,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h2 key={monster.id}>{monster.name}</h2>
-        ))}
+        <CardList>
+          {this.state.monsters.map(monster => (
+            <h2 key={monster.id}>{monster.name}</h2>
+          ))}
+        </CardList>
       </div>
     );
   }
