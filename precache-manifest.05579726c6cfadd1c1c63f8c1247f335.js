b4880963799efce8b28f562890086e8a902cf58f
Henrique S.A <henriqueator@gmail.com>
2020-07-16 21:06:08 -0300

    Updates

---
+++ precache-manifest.05579726c6cfadd1c1c63f8c1247f335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee553ead66a922517ddb7e41ec493fc",
+    "url": "/robots-reactjs/index.html"
+  },
+  {
+    "revision": "cd4ec68638c9b54dfcf2",
+    "url": "/robots-reactjs/static/css/main.abc231e8.chunk.css"
+  },
+  {
+    "revision": "9e190a8ff2d617a8ec21",
+    "url": "/robots-reactjs/static/js/2.c291a8ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots-reactjs/static/js/2.c291a8ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4ec68638c9b54dfcf2",
+    "url": "/robots-reactjs/static/js/main.59743e9d.chunk.js"
+  },
+  {
+    "revision": "54d08a50bcaa627dcfbf",
+    "url": "/robots-reactjs/static/js/runtime-main.cb7f59af.js"
+  }
+]);
